a894df116f2b48d0205d160fd64c34933b4082cf
Ronny Vega <ronny_vega@icloud.com>
2016-03-06 20:12:17 -0800

    added Realm frameworks manually

---
+++ Life Index/LifeImage.swift
@@ -0,0 +1,19 @@
+//
+//  LifeImage.swift
+//  Life Index
+//
+//  Created by Atomisk on 3/3/16.
+//  Copyright © 2016 CS125. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class LifeImage: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
